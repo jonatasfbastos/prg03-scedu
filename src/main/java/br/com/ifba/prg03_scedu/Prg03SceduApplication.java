d96f7504be656b44b7de4b056f256725ab30d69c
Rafael Andrade <devrafaelandradeoliveira@gmail.com>
2024-08-15 16:45:26 -0300

    Project created

---
+++ src/main/java/br/com/ifba/prg03_scedu/Prg03SceduApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifba.prg03_scedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prg03SceduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prg03SceduApplication.class, args);
+	}
+
+}
