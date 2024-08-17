0172c579afc33b797deddcd435f1d6493812109b
FlavioJr0010 <juniornordeste10@hotmail.com>
2024-08-17 19:27:26 -0300

    Inserções na main do codigo 17/08

--- src/main/java/br/com/ifba/prg03_scedu/Prg03SceduApplication.java
+++ src/main/java/br/com/ifba/prg03_scedu/Prg03SceduApplication.java
@@ -2,12 +2,25 @@ package br.com.ifba.prg03_scedu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Prg03SceduApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Prg03SceduApplication.class, args);
+            
+            // Cria um contexto de aplicação Spring com a classe Application como configuração principal
+        // O método headless(false) indica que a aplicação pode interagir com um ambiente gráfico
+        ConfigurableApplicationContext context = 
+            new SpringApplicationBuilder(Prg03SceduApplication.class)
+            .headless(false)
+            .run(args);
+
+        /*// Obtém o bean CursoListar do contexto Spring e o torna visível
+        CursoListar telaListar = context.getBean(CursoListar.class);
+        telaListar.setVisible(true);
+		SpringApplication.run(Prg03SceduApplication.class, args);*/
 	}
 
 }
