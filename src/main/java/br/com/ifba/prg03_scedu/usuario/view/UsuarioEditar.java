5d6dc02f4b34aeec83869ccbd7e1c2ceae8fccab
alefm <alefmontino.p@gmail.com>
2024-08-20 12:13:57 -0300

    Fix: Remoção dos icones das telas Cadastro e Editar. Builder: Em construção lógica de segurança de senha nas regras de negócio.

--- src/main/java/br/com/ifba/prg03_scedu/usuario/view/UsuarioEditar.java
+++ src/main/java/br/com/ifba/prg03_scedu/usuario/view/UsuarioEditar.java
@@ -40,15 +40,12 @@ public class UsuarioEditar extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         emailTextfd = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jLabel11 = new javax.swing.JLabel();
         senhaTextfd = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         voltarButtomd = new javax.swing.JButton();
         salvarButtomd = new javax.swing.JButton();
         editarComboBox = new javax.swing.JComboBox<>();
-        PasswordFieldEditar = new javax.swing.JPasswordField();
-        verButtomEd = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -64,9 +61,6 @@ public class UsuarioEditar extends javax.swing.JFrame {
         emailTextfd.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
 
         jLabel2.setText("Email:");
-
-        jLabel11.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/editcad.png"))); // NOI18N
-        jLabel11.setText("jLabel11");
 
         senhaTextfd.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         senhaTextfd.addActionListener(new java.awt.event.ActionListener() {
@@ -102,27 +96,10 @@ public class UsuarioEditar extends javax.swing.JFrame {
         editarComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Administrador", "Gerente", "Suporte", "Convidado" }));
         editarComboBox.setBorder(null);
 
-        PasswordFieldEditar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                PasswordFieldEditarActionPerformed(evt);
-            }
-        });
-
-        verButtomEd.setText("ver");
-        verButtomEd.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                verButtomEdActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(141, 141, 141))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(50, 50, 50)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
@@ -146,23 +123,15 @@ public class UsuarioEditar extends javax.swing.JFrame {
                 .addContainerGap(50, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(107, 107, 107)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(salvarButtomd, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(voltarButtomd, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(PasswordFieldEditar, javax.swing.GroupLayout.PREFERRED_SIZE, 234, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(verButtomEd, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addGap(0, 0, Short.MAX_VALUE))
+                .addComponent(salvarButtomd, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(voltarButtomd, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 110, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(31, 31, 31)
-                .addComponent(jLabel11, javax.swing.GroupLayout.PREFERRED_SIZE, 153, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(27, 27, 27)
+                .addGap(211, 211, 211)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(nomeTextfc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
@@ -178,11 +147,7 @@ public class UsuarioEditar extends javax.swing.JFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
                     .addComponent(editarComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(PasswordFieldEditar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(verButtomEd))
-                .addGap(43, 43, 43)
+                .addGap(84, 84, 84)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(voltarButtomd, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(salvarButtomd, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -201,9 +166,9 @@ public class UsuarioEditar extends javax.swing.JFrame {
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(25, Short.MAX_VALUE)
+                .addContainerGap(23, Short.MAX_VALUE)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(23, 23, 23))
+                .addGap(25, 25, 25))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -296,14 +261,6 @@ public class UsuarioEditar extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_salvarButtomdActionPerformed
 
-    private void verButtomEdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_verButtomEdActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_verButtomEdActionPerformed
-
-    private void PasswordFieldEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_PasswordFieldEditarActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_PasswordFieldEditarActionPerformed
-
     private void senhaTextfdActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_senhaTextfdActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_senhaTextfdActionPerformed
@@ -319,38 +276,17 @@ public class UsuarioEditar extends javax.swing.JFrame {
     }
    
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPasswordField PasswordFieldEditar;
-    private javax.swing.JButton btnCriarConta;
-    private javax.swing.JButton btnCriarConta1;
-    private javax.swing.JButton btnEntrar;
-    private javax.swing.JButton btnEntrar1;
     private javax.swing.JComboBox<String> editarComboBox;
     private javax.swing.JTextField emailTextfd;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
-    private javax.swing.JLayeredPane jLayeredPane1;
-    private javax.swing.JLayeredPane jLayeredPane2;
-    private javax.swing.JLayeredPane jLayeredPane3;
-    private javax.swing.JLayeredPane jLayeredPane4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JTextField nomeTextfc;
     private javax.swing.JButton salvarButtomd;
     private javax.swing.JTextField senhaTextfd;
-    private javax.swing.JTextField tfEmail;
-    private javax.swing.JTextField tfEmail1;
-    private javax.swing.JTextField tfSenha;
-    private javax.swing.JTextField tfSenha1;
-    private javax.swing.JButton verButtomEd;
     private javax.swing.JButton voltarButtomd;
     // End of variables declaration//GEN-END:variables
 }
