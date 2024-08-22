291fe8fdc8d1ebc6dcbe4a5506fe5f87f9b3e33b
alefm <alefmontino.p@gmail.com>
2024-08-22 00:10:58 -0300

    Update: Implementação de um Textfield, para realizar a confirmação de senha.

--- src/main/java/br/com/ifba/prg03_scedu/usuario/view/UsuarioCadastrar.java
+++ src/main/java/br/com/ifba/prg03_scedu/usuario/view/UsuarioCadastrar.java
@@ -34,6 +34,9 @@ public class UsuarioCadastrar extends javax.swing.JFrame {
         cadastrarComboBox = new javax.swing.JComboBox<>();
         jLabel4 = new javax.swing.JLabel();
         voltarButtomc = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        senhaConfirmTextfc = new javax.swing.JTextField();
+        jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -53,14 +56,22 @@ public class UsuarioCadastrar extends javax.swing.JFrame {
             }
         });
 
+        jLabel1.setBackground(new java.awt.Color(204, 204, 204));
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel1.setText("Nome:");
 
+        jLabel2.setBackground(new java.awt.Color(204, 204, 204));
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel2.setText("Email:");
 
+        jLabel3.setBackground(new java.awt.Color(204, 204, 204));
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel3.setText("Senha:");
 
         cadastrarComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Administrador", "Gerente", "Suporte", "Convidado" }));
 
+        jLabel4.setBackground(new java.awt.Color(204, 204, 204));
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel4.setText("Nível de Acesso:");
 
         voltarButtomc.setText("Voltar");
@@ -71,42 +82,68 @@ public class UsuarioCadastrar extends javax.swing.JFrame {
             }
         });
 
+        jLabel5.setBackground(new java.awt.Color(204, 204, 204));
+        jLabel5.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(204, 204, 204));
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("Cadastro de Usuários");
+
+        senhaConfirmTextfc.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                senhaConfirmTextfcActionPerformed(evt);
+            }
+        });
+
+        jLabel6.setBackground(new java.awt.Color(204, 204, 204));
+        jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLabel6.setText("Confirmação de Senha:");
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(41, 41, 41)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGap(111, 111, 111)
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(105, 105, 105)
+                .addComponent(cadastrarButtomc, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(voltarButtomc, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(50, 50, 50)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                        .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(cadastrarComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(jPanel2Layout.createSequentialGroup()
-                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(senhaTextfc, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(jPanel2Layout.createSequentialGroup()
-                            .addGap(69, 69, 69)
-                            .addComponent(cadastrarButtomc, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(voltarButtomc, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(emailTextfc, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(jPanel2Layout.createSequentialGroup()
-                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addComponent(nomeTextfc, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(58, Short.MAX_VALUE))
+                            .addComponent(jLabel1))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(emailTextfc, javax.swing.GroupLayout.DEFAULT_SIZE, 275, Short.MAX_VALUE)
+                            .addComponent(nomeTextfc))
+                        .addGap(0, 80, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel6)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addGap(1, 1, 1)
+                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 44, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)
+                                .addComponent(senhaTextfc, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel2Layout.createSequentialGroup()
+                                    .addComponent(jLabel4)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(cadastrarComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 204, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(senhaConfirmTextfc, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 338, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(79, 79, 79))))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addContainerGap(203, Short.MAX_VALUE)
+                .addGap(63, 63, 63)
+                .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 62, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(nomeTextfc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
@@ -118,15 +155,19 @@ public class UsuarioCadastrar extends javax.swing.JFrame {
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(senhaTextfc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3))
+                .addGap(10, 10, 10)
+                .addComponent(jLabel6)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(senhaConfirmTextfc, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel4)
                     .addComponent(cadastrarComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(89, 89, 89)
+                .addGap(68, 68, 68)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(voltarButtomc, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(cadastrarButtomc, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(48, 48, 48))
+                .addGap(49, 49, 49))
         );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -163,58 +204,55 @@ public class UsuarioCadastrar extends javax.swing.JFrame {
     private void cadastrarButtomcActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cadastrarButtomcActionPerformed
         // TODO add your handling code here:
             try {
-              // Capturando os valores dos campos de texto e spinner
-              String nome = nomeTextfc.getText();
-              String email = emailTextfc.getText();
-              String senha = senhaTextfc.getText();
-              String nivelAcesso = (String) cadastrarComboBox.getSelectedItem();
+                // Capturando os valores dos campos de texto
+                String nome = nomeTextfc.getText();
+                String email = emailTextfc.getText();
+                String senha = senhaTextfc.getText();
+                String confirmarSenha = senhaConfirmTextfc.getText();
+                String nivelAcesso = (String) cadastrarComboBox.getSelectedItem();
 
-              // Verifica se o e-mail já está em uso
-              if (usuarioController.existsByEmail(email)) {
-                  JOptionPane.showMessageDialog(this, 
-                      "O e-mail já está em uso. Informe um e-mail diferente.", 
-                      "E-mail Duplicado", 
-                      JOptionPane.WARNING_MESSAGE
-                  );
-                  return; // Sai do método para evitar salvar o usuário com e-mail duplicado
-              }
+                // Validação de confirmação de senha
+                if (!senha.equals(confirmarSenha)) {
+                    JOptionPane.showMessageDialog(this, "As senhas não coincidem.", "Erro", JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
 
-              // Criando um objeto Usuario com os valores capturados
-              Usuario usuario = new Usuario();
-              usuario.setNome(nome);
-              usuario.setEmail(email);
-              usuario.setSenha(senha);
-              usuario.setNivelAcesso(nivelAcesso);
+                // Criando um objeto Usuario com os valores capturados
+                Usuario usuario = new Usuario();
+                usuario.setNome(nome);
+                usuario.setEmail(email);
+                usuario.setSenha(senha);
+                usuario.setNivelAcesso(nivelAcesso);
 
-              // Chamando o método save do controller para salvar o usuário
-              usuarioController.save(usuario);
+                // Chamando o método save do controller para salvar o usuário
+                usuarioController.save(usuario);
 
-              // Exibindo mensagem de sucesso
-              JOptionPane.showMessageDialog(this, "Usuário cadastrado com sucesso!");
+                // Exibindo mensagem de sucesso
+                JOptionPane.showMessageDialog(this, "Usuário cadastrado com sucesso!");
 
-              // Atualiza a tabela de usuários na tela UsuarioListar, se estiver aberta
-              UsuarioListar usuarioListar = null;
-              for (java.awt.Window window : java.awt.Window.getWindows()) {
-                  if (window instanceof UsuarioListar) {
-                      usuarioListar = (UsuarioListar) window;
-                      break;
-                  }
-              }
-              if (usuarioListar != null) {
-                  usuarioListar.carregarUsuarios();
-              }
+                // Atualiza a tabela de usuários na tela UsuarioListar, se estiver aberta
+                UsuarioListar usuarioListar = null;
+                for (java.awt.Window window : java.awt.Window.getWindows()) {
+                    if (window instanceof UsuarioListar) {
+                        usuarioListar = (UsuarioListar) window;
+                        break;
+                    }
+                }
+                if (usuarioListar != null) {
+                    usuarioListar.carregarUsuarios();
+                }
 
-              // Fechar a tela de cadastro após o sucesso
-              this.dispose();
+                // Fechar a tela de cadastro após o sucesso
+                this.dispose();
 
-          } catch (Exception e) {
-              // Exibindo mensagem de erro caso ocorra uma exceção
-              JOptionPane.showMessageDialog(this, 
-                  "Erro ao cadastrar o usuário: " + e.getMessage(), 
-                  "Erro", 
-                  JOptionPane.ERROR_MESSAGE
-              );
-          }
+            } catch (Exception e) {
+                // Exibindo mensagem de erro caso ocorra uma exceção
+                JOptionPane.showMessageDialog(this, 
+                    "Erro ao cadastrar o usuário: " + e.getMessage(), 
+                    "Erro", 
+                    JOptionPane.ERROR_MESSAGE
+                );
+            }
     }//GEN-LAST:event_cadastrarButtomcActionPerformed
 
     private void voltarButtomcActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_voltarButtomcActionPerformed
@@ -233,6 +271,10 @@ public class UsuarioCadastrar extends javax.swing.JFrame {
                     usuarioListar.carregarUsuarios();
                 }
     }//GEN-LAST:event_voltarButtomcActionPerformed
+
+    private void senhaConfirmTextfcActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_senhaConfirmTextfcActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_senhaConfirmTextfcActionPerformed
     
     public static void main(String args[]) {
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -253,9 +295,12 @@ public class UsuarioCadastrar extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JTextField nomeTextfc;
+    private javax.swing.JTextField senhaConfirmTextfc;
     private javax.swing.JTextField senhaTextfc;
     private javax.swing.JButton voltarButtomc;
     // End of variables declaration//GEN-END:variables
