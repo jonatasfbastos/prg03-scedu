27f48c894f38f06005e70d8cc74524eb81e927fb
Vittorio Kevin <95937607+Vittoriosrn@users.noreply.github.com>
2024-08-24 15:08:12 -0300

    teste

--- src/main/java/br/com/ifba/infrastructure/entity/Pessoa.java
+++ src/main/java/br/com/ifba/infrastructure/entity/Pessoa.java
@@ -1,5 +1,14 @@
 package br.com.ifba.infrastructure.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import java.util.Calendar;
 import jakarta.persistence.*;
 import java.util.Date;
 import lombok.Data;
