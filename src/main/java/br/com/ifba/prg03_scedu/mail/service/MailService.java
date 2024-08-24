19c0cc94e8ad19c4499f726193c6ee226cb8759a
geodds <geo.dourado12@gmail.com>
2024-08-24 09:51:13 -0300

    Resolvendo conflito no pom

--- src/main/java/br/com/ifba/prg03_scedu/mail/service/MailService.java
+++ src/main/java/br/com/ifba/prg03_scedu/mail/service/MailService.java
@@ -8,6 +8,8 @@ import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 /**
  *
